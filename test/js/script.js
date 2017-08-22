25293018d7fb961d00e187ec18959a8ca0095cbb
AnarAbishov <mr.avinar@mail.ru>
2017-08-22 16:50:03 +0400

    Add files via upload

---
+++ test/js/script.js
@@ -0,0 +1,11 @@
+var tag = document.getElementsByTagName("p")[0].innerHTML = "Salam necesen";
+
+var tag2 = document.querySelector("p").innerHTML = "Salam baku";
+
+var saat = document.write(Date());
+
+var image = document.getElementById("image").src = "2.jpg";
+
+var tag3 = document.querySelector("p").style.color = "blue";
+
+var button = onclick=document.querySelector("button").style.color = "red";
